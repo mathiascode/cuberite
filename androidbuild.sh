b584e29cf6a5a11dbc99e779378c9b2dee1a76c8
Mat <mail@mathias.is>
2019-11-22 02:51:19 +0200

    Update androidbuild.sh

--- androidbuild.sh
+++ androidbuild.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 curl -LO https://dl.google.com/android/repository/android-ndk-r13b-linux-x86_64.zip
 unzip -qo android-ndk*
-NDK="android-ndk-r13b/" CMAKE="/usr/local/bin/cmake" android/compile.sh all
+NDK="android-ndk-r13b/" CMAKE=cmake android/compile.sh all
 mkdir build
 mv Server/*.zip build/
 #
