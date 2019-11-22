87f50d83ab89f19097598b3000de4bef695cd2bc
Mat <mail@mathias.is>
2019-11-22 02:26:00 +0200

    Create androidbuild.sh

---
+++ androidbuild.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+curl -LO https://dl.google.com/android/repository/android-ndk-r13b-linux-x86_64.zip
+unzip -o android-ndk*
+NDK="android-ndk-r13b/" CMAKE="/usr/bin/cmake" android/compile.sh all
